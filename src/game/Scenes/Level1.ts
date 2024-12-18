01191299d3ac6938677d7fbe10d33e25d0f29df1
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2024-12-18 14:02:39 +0700

    fix: resolution

--- src/game/Scenes/Level1.ts
+++ src/game/Scenes/Level1.ts
@@ -3,34 +3,47 @@ import { Player } from "../Actors/Player";
 import { Enemy } from "../Actors/Enemy";
 
 export class Level1 extends Scene {
+  player: Player = new Player({
+    width: 16,
+    height: 16,
+  });
   constructor() {
     super();
   }
   override onInitialize(engine: Engine): void {
     console.log("hello");
-    const player = new Player({
-      pos: vec(64, engine.drawHeight - 8),
-      width: 16,
-      height: 16,
-      color: Color.Red,
-    });
-    this.add(player);
-    let enemies = getEnemies();
+    this.player.pos = vec(64, engine.drawHeight - 8);
+    this.add(this.player);
+    let enemies = createEnemyPack(2);
     enemies.forEach((e) => this.add(e));
+    console.log(this.entities);
+  }
+  override onPostUpdate(engine: Engine, elapsed: number): void {
+    if (this.entities.filter((e) => e instanceof Enemy).length === 0) {
+      this.player.actions
+        .moveTo(vec(engine.halfDrawWidth - 8, engine.drawHeight - 8), 94)
+        .moveTo(vec(engine.halfDrawWidth - 8, -8), 128)
+        .callMethod(() => {
+          engine.goToScene("menu");
+        });
+    }
   }
 }
 
-function getEnemies() {
+// TODO: customize enemy pack rows plane, [gava, borg, supergava]
+function createEnemyPack(rows: number) {
   let enemies: Enemy[] = [];
-  for (let i = 8; i < 16 * 8; i += 16) {
-    enemies.push(
-      new Enemy({
-        pos: vec(i, 16),
-        width: 16,
-        height: 16,
-        vel: vec(32, 0),
-      }),
-    );
+  for (let j = 32; j < 32 + 16 * rows; j += 16) {
+    for (let i = 8; i < 24 * 8; i += 24) {
+      enemies.push(
+        new Enemy({
+          pos: vec(i, j),
+          width: 16,
+          height: 16,
+          vel: vec(32, 0),
+        }),
+      );
+    }
   }
   return enemies;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,45 @@
 import { loader } from "./game/Resource";
 import { Level1 } from "./game/Scenes/Level1";
+import { Menu } from "./game/Scenes/Menu";
 import "./style.css";
 import { Color, Engine, Resolution } from "excalibur";
 
 // TODO: bullets
 
+const config = {
+  scale: 3,
+  resWidth: 320,
+  resHeight: 224,
+  get vw() {
+    return this.resWidth * this.scale;
+  },
+  get vh() {
+    return this.resHeight * this.scale;
+  },
+};
+
+// TODO: change this to resizeobserver
+if (window.innerWidth <= 960) {
+  config.scale = 2;
+}
+if (window.innerWidth <= 640) {
+  config.scale = 1;
+}
+
 const game = new Engine({
-  viewport: { width: 800, height: 600 },
-  resolution: Resolution.GameBoyAdvance,
+  viewport: { width: config.vw, height: config.vh },
+  resolution: { width: config.resWidth, height: config.resHeight },
   maxFps: 60,
   fixedUpdateFps: 60,
   pixelArt: true,
   antialiasing: false,
   backgroundColor: Color.Black,
+  canvasElementId: "canvas",
 });
+
 const lvl1 = new Level1();
+const menu = new Menu();
 game.add("level1", lvl1);
-game.goToScene("level1");
+game.add("menu", menu);
+game.goToScene("menu");
 game.start(loader);
