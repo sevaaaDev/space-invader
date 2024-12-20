660c15cd2eb43a7dba3fb49cc8ccbcf8f71cd42f
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2024-12-20 13:47:46 +0700

    fix: player state

--- src/game/Scenes/Level1.ts
+++ src/game/Scenes/Level1.ts
@@ -1,30 +1,31 @@
 import { Color, Engine, Scene, vec } from "excalibur";
-import { Player } from "../Actors/Player";
+import { player, Player } from "../Actors/Player";
 import { Enemy } from "../Actors/Enemy";
 import { createEnemyPack } from "../Utils/createEnemyPack";
 
 export class Level1 extends Scene {
-  player: Player = new Player({
-    width: 16,
-    height: 16,
-  });
   constructor() {
     super();
   }
   override onInitialize(engine: Engine): void {
     console.log("hello");
-    this.player.pos = vec(64, engine.drawHeight - 8);
-    this.add(this.player);
+    player.pos = vec(64, engine.drawHeight - 8);
+    this.add(player);
     let enemies = createEnemyPack(2, ["Gava", "Gava"]);
     console.log(enemies);
     enemies.forEach((e) => this.add(e));
     console.log(this.entities);
   }
   override onPostUpdate(engine: Engine, elapsed: number): void {
+    if (this.entities.filter((e) => e instanceof Player).length === 0) {
+      // TODO: create game over menu
+      engine.goToScene("menu");
+      return;
+    }
     if (this.entities.filter((e) => e instanceof Enemy).length === 0) {
-      this.player.actions
+      player.actions
         .moveTo(vec(engine.halfDrawWidth - 8, engine.drawHeight - 8), 94)
-        .moveTo(vec(engine.halfDrawWidth - 8, -8), 128)
+        .moveTo(vec(engine.halfDrawWidth - 8, 16), 128)
         .callMethod(() => {
           engine.goToScene("menu");
         });
--- src/game/Scenes/Level2.ts
+++ src/game/Scenes/Level2.ts
@@ -1,27 +1,26 @@
 import { Color, Engine, Scene, vec } from "excalibur";
-import { Player } from "../Actors/Player";
+import { player, Player } from "../Actors/Player";
 import { Enemy } from "../Actors/Enemy";
 import { createEnemyPack } from "../Utils/createEnemyPack";
 
 export class Level2 extends Scene {
-  player: Player = new Player({
-    width: 16,
-    height: 16,
-  });
   constructor() {
     super();
   }
   override onInitialize(engine: Engine): void {
     console.log("hello");
-    this.player.pos = vec(64, engine.drawHeight - 8);
-    this.add(this.player);
+    player.pos = vec(64, engine.drawHeight - 8);
+    // FIX: have to call this vv
+    player.actions.clearActions();
+    this.add(player);
     let enemies = createEnemyPack(2, ["Gava", "Borg"]);
     enemies.forEach((e) => this.add(e));
     console.log(this.entities);
   }
   override onPostUpdate(engine: Engine, elapsed: number): void {
     if (this.entities.filter((e) => e instanceof Enemy).length === 0) {
-      this.player.actions
+      console.log("actions");
+      player.actions
         .moveTo(vec(engine.halfDrawWidth - 8, engine.drawHeight - 8), 94)
         .moveTo(vec(engine.halfDrawWidth - 8, -8), 128)
         .callMethod(() => {
