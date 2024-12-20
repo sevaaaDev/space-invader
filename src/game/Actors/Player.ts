9510c8b0d98c6b64b065648058fdac627e89487a
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2024-12-20 13:47:31 +0700

    feat: player limit shot interval

--- src/game/Actors/Player.ts
+++ src/game/Actors/Player.ts
@@ -1,15 +1,25 @@
-import { Actor, ActorArgs, Engine, Keys, Vector } from "excalibur";
+import { Actor, ActorArgs, Engine, Keys, Timer, Vector } from "excalibur";
 import { Resource } from "../Resource";
 import { PlayerBullet } from "./PlayerBullet";
 
 export class Player extends Actor {
   public health: number = 3;
+  private _hasShot: boolean = false;
+  private _timer: Timer = new Timer({
+    fcn: () => {
+      this._hasShot = false;
+      this._timer.reset();
+    },
+    interval: 1000,
+  });
   constructor(props: ActorArgs) {
     super({ ...props });
   }
   override onInitialize(_engine: Engine): void {
     this.addTag("player");
     this.graphics.use(Resource.player.toSprite());
+    // FIX: timer doesnt get add on next level
+    this.scene?.addTimer(this._timer);
   }
   override onPreUpdate(engine: Engine, _elapsed: number): void {
     if (engine.input.keyboard.isHeld(Keys.L)) {
@@ -20,12 +30,14 @@ export class Player extends Actor {
       this.vel.x = -64;
       return;
     }
-    if (engine.input.keyboard.wasPressed(Keys.Space)) {
+    if (engine.input.keyboard.wasPressed(Keys.Space) && !this._hasShot) {
+      this._hasShot = true;
       engine.add(
         new PlayerBullet({
           pos: this.pos.clone().add(new Vector(0, -this.height / 2)),
         }),
       );
+      this._timer.start();
     }
     this.vel.x = 0;
   }
@@ -37,3 +49,8 @@ export class Player extends Actor {
     }
   }
 }
+
+export const player = new Player({
+  width: 16,
+  height: 16,
+});
