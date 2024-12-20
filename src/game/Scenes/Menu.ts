53c24ed6a6c399d3185dc453652f88cf2106c265
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2024-12-20 12:58:18 +0700

    fix: menu

--- src/game/Scenes/Menu.ts
+++ src/game/Scenes/Menu.ts
@@ -1,39 +1,41 @@
 import { Engine, Scene, SceneActivationContext, Timer } from "excalibur";
+import { gameState } from "../State";
 
 const ui: HTMLDivElement | null = document.querySelector(".ui");
-// FIX: doesnt countdown after second activation
 export class Menu extends Scene {
-  countdown: Timer = new Timer({
+  countdownTimer: Timer = new Timer({
     fcn: () => {
       console.log("h");
-      const count: HTMLParagraphElement | null =
-        document.querySelector(".count");
-      count!.innerText = `${this.count--}`;
+      if (this.count === 0) {
+        this.count = 2;
+        gameState.setState("currentLevel", 2);
+        this.engine.goToScene(`level${gameState.state.currentLevel}`);
+        return;
+      }
+      this.countdownElement.innerText = `${this.count--}`;
     },
     numberOfRepeats: 3,
     interval: 1000,
     repeats: true,
   });
   count: number = 2;
+  countdownElement: HTMLParagraphElement = document.createElement("p");
   override onInitialize(engine: Engine): void {
-    this.add(this.countdown);
+    this.add(this.countdownTimer);
   }
   override onActivate(context: SceneActivationContext<unknown>): void {
     const text = document.createElement("p");
     text.innerText = "Next Wave";
-    const count = document.createElement("p");
-    count.classList.add("count");
-    count.innerText = "3";
+    this.countdownElement.innerText = "3";
     ui?.appendChild(text);
-    ui?.appendChild(count);
-    this.countdown.start();
-    this.countdown.on(() => {
-      if (this.count === -1) {
-        this.engine.goToScene("level1");
-      }
-    });
+    ui?.appendChild(this.countdownElement);
+    this.countdownTimer.reset();
+    this.countdownTimer.start();
   }
   override onDeactivate(context: SceneActivationContext): void {
-    ui!.innerHTML = "";
+    if (ui === null) {
+      throw "Ui is null, maybe u forgot to add the element";
+    }
+    ui.innerHTML = "";
   }
 }
