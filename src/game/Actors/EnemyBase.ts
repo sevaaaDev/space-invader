8a0b7318fbc06e61d413f3a38be4d2934924c49b
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2024-12-24 13:00:18 +0700

    fix: add enemy base class

---
+++ src/game/Actors/EnemyBase.ts
@@ -0,0 +1,77 @@
+import {
+  Actor,
+  ActorArgs,
+  Collider,
+  CollisionContact,
+  Engine,
+  EventEmitter,
+  Graphic,
+  Random,
+  Scene,
+  Side,
+  Sprite,
+  Subscription,
+  Timer,
+  Vector,
+} from "excalibur";
+import { PlayerBullet } from "./PlayerBullet";
+import { vent } from "./enemy/event";
+
+const random = new Random(999);
+export class EnemyBase extends Actor {
+  private _accTime: number = 0;
+  private _handler: Subscription | null = null;
+  private _sprite: Graphic | undefined;
+  private _shootTimer: Timer = new Timer({
+    fcn: () => {
+      this.shoot();
+    },
+    interval: 3000,
+    random,
+    randomRange: [1000, 10000],
+    repeats: true,
+  });
+  constructor(props: ActorArgs & { sprite?: Graphic }) {
+    super(props);
+    this._sprite = props.sprite;
+  }
+  shoot() {
+    // override
+  }
+  hit() {
+    this.scene!.checkWinning();
+    this.actions.blink(100, 100, 1).die();
+  }
+  override onInitialize() {
+    if (this._sprite) {
+      this.graphics.use(this._sprite);
+    }
+    this._handler = vent.on("reversedirection", (e) => {
+      if (Math.sign(this.vel.x) === e.sign) return;
+      this.vel.x *= -1;
+    });
+    this.scene?.addTimer(this._shootTimer);
+    this._shootTimer.start();
+  }
+  override onPreUpdate(engine: Engine, elapsed: number): void {
+    if (this.pos.x + this.width / 2 > engine.drawWidth) {
+      vent.emit("reversedirection", { sign: -1 });
+    }
+    if (this.pos.x - this.width / 2 < 0) {
+      vent.emit("reversedirection", { sign: 1 });
+    }
+  }
+  override onCollisionStart(
+    _self: Collider,
+    other: Collider,
+    _side: Side,
+    _contact: CollisionContact,
+  ): void {
+    if (other.owner instanceof PlayerBullet) {
+      this.hit();
+    }
+  }
+  override onPreKill(_scene: Scene): void {
+    this._handler?.close();
+  }
+}
--- src/game/Actors/enemy/SuperGava.ts
+++ src/game/Actors/enemy/SuperGava.ts
@@ -17,76 +17,35 @@ import {
 import { PlayerBullet } from "../PlayerBullet";
 import { EnemyBullet } from "../EnemyBullet";
 import { vent } from "./event";
+import { EnemyBase } from "../EnemyBase";
 
 const random = new Random(999);
-export class SuperGava extends Actor {
+export class SuperGava extends EnemyBase {
   public health: number = 2;
-  private _accTime: number = 0;
-  private _handler: Subscription | null = null;
-  private _sprite: Graphic | undefined;
-  private _shootTimer: Timer = new Timer({
-    fcn: () => {
-      this.scene?.engine.add(
-        new EnemyBullet({
-          pos: this.pos.clone().add(new Vector(4, this.height / 2)),
-          vel: new Vector(0, 32),
-          width: 1,
-          height: 4,
-        }),
-      );
-      this.scene?.engine.add(
-        new EnemyBullet({
-          pos: this.pos.clone().add(new Vector(-4, this.height / 2)),
-          vel: new Vector(0, 32),
-          width: 1,
-          height: 4,
-        }),
-      );
-    },
-    interval: 3000,
-    random,
-    randomRange: [1000, 10000],
-    repeats: true,
-  });
-  constructor(props: ActorArgs & { sprite?: Graphic }) {
-    super(props);
-    this._sprite = props.sprite;
+  override shoot(): void {
+    this.scene?.engine.add(
+      new EnemyBullet({
+        pos: this.pos.clone().add(new Vector(4, this.height / 2)),
+        vel: new Vector(0, 32),
+        width: 1,
+        height: 4,
+      }),
+    );
+    this.scene?.engine.add(
+      new EnemyBullet({
+        pos: this.pos.clone().add(new Vector(-4, this.height / 2)),
+        vel: new Vector(0, 32),
+        width: 1,
+        height: 4,
+      }),
+    );
   }
-  override onInitialize() {
-    if (this._sprite) {
-      this.graphics.use(this._sprite);
+  override hit(): void {
+    this.health--;
+    this.actions.blink(100, 100, 1);
+    if (this.health === 0) {
+      this.scene!.checkWinning();
+      this.actions.blink(100, 100, 1).die();
     }
-    this._handler = vent.on("reversedirection", (e: any) => {
-      if (Math.sign(this.vel.x) === e.sign) return;
-      this.vel.x *= -1;
-    });
-    this.scene?.addTimer(this._shootTimer);
-    this._shootTimer.start();
-  }
-  override onPreUpdate(engine: Engine, elapsed: number): void {
-    if (this.pos.x + this.width / 2 > engine.drawWidth) {
-      vent.emit("reversedirection", { sign: -1 });
-    }
-    if (this.pos.x - this.width / 2 < 0) {
-      vent.emit("reversedirection", { sign: 1 });
-    }
-  }
-  override onCollisionStart(
-    _self: Collider,
-    other: Collider,
-    _side: Side,
-    _contact: CollisionContact,
-  ): void {
-    if (other.owner instanceof PlayerBullet) {
-      this.health--;
-      this.actions.blink(100, 100, 1);
-      if (this.health === 0) {
-        this.actions.blink(100, 100, 1).die();
-        this.scene!.checkWinning();
-      }
-    }
-  }
-  override onPreKill(_scene: Scene): void {
-    this._handler?.close();
   }
 }
