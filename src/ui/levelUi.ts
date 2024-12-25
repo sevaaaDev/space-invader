823a9d9f1e7b99860041967053e518e2db3e2390
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2024-12-25 18:10:04 +0700

    fix: font is weird

--- src/ui/levelUi.ts
+++ src/ui/levelUi.ts
@@ -2,5 +2,5 @@ export function updateLevelUI(level: number): void {
   const levelUi: HTMLParagraphElement | null =
     document.querySelector(".level-ui");
   if (levelUi === null) throw "level ui is not found";
-  levelUi.innerText = "Level " + level;
+  levelUi.innerText = "LEVEL " + level;
 }
