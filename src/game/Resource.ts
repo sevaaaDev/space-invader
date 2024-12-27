8fb4951c248382d87b3c11ba2bb25bdb89e0e116
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2024-12-27 13:38:37 +0700

    fix: path

--- src/game/Resource.ts
+++ src/game/Resource.ts
@@ -1,13 +1,20 @@
 import { DefaultLoader, ImageSource, SpriteSheet } from "excalibur";
+import playerPath from "/player.png";
+import gavaSheet from "/gava_spritesheet.png";
+import gava from "/gava_f_95.png";
+import borg from "/borg_a_60.png";
+import superGava from "/super_gava.png";
+import borgSheet from "/borg_spritesheet.png";
+import superGavaSheet from "/supergava_spritesheet.png";
 
 export const Resource = {
-  player: new ImageSource("player.png"),
-  gava: new ImageSource("gava_f_95.png"),
-  superGava: new ImageSource("super_gava.png"),
-  borg: new ImageSource("borg_a_60.png"),
-  gavaSheet: new ImageSource("gava_spritesheet.png"),
-  borgSheet: new ImageSource("borg_spritesheet.png"),
-  superGavaSheet: new ImageSource("supergava_spritesheet.png"),
+  player: new ImageSource(playerPath),
+  gava: new ImageSource(gava),
+  superGava: new ImageSource(superGava),
+  borg: new ImageSource(borg),
+  gavaSheet: new ImageSource(gavaSheet),
+  borgSheet: new ImageSource(borgSheet),
+  superGavaSheet: new ImageSource(superGavaSheet),
 } as const;
 
 // TODO: change this to custom loader
--- src/ui/healtbar.ts
+++ src/ui/healtbar.ts
@@ -1,10 +1,11 @@
+import imgPlayer from "/player.png";
 export function updateHealthBar(health: number): void {
   const healthBarEl: HTMLDivElement | null = document.querySelector(".health");
   if (healthBarEl === null) throw "health bar not found";
   healthBarEl.innerHTML = "";
   for (let i = 0; i < health - 1; i++) {
     const img = document.createElement("img");
-    img.src = "player.png";
+    img.src = imgPlayer;
     healthBarEl.appendChild(img);
   }
 }
