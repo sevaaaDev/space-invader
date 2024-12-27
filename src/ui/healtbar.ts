570af3d034f4665cdd5fd6f7c8027e9930c61c2e
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2024-12-27 12:54:26 +0700

    fix: health bar doesnt show correct health

--- src/ui/healtbar.ts
+++ src/ui/healtbar.ts
@@ -2,7 +2,7 @@ export function updateHealthBar(health: number): void {
   const healthBarEl: HTMLDivElement | null = document.querySelector(".health");
   if (healthBarEl === null) throw "health bar not found";
   healthBarEl.innerHTML = "";
-  for (let i = 0; i < health; i++) {
+  for (let i = 0; i < health - 1; i++) {
     const img = document.createElement("img");
     img.src = "player.png";
     healthBarEl.appendChild(img);
